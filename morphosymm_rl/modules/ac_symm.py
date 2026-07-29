fa434db06e07d0ed92d81044213e263310a9a6be
Evelyn D'Elia <17877131+evelyd@users.noreply.github.com>
2026-07-29 17:29:04 +0000

    switch to non-equiv normal dist and simpleEMLP for actor/critic

--- morphosymm_rl/modules/ac_symm.py
+++ morphosymm_rl/modules/ac_symm.py
@@ -65,25 +65,35 @@ class ActorCriticSymm(ActorCritic):
         self.actor_out_type = FieldType(gspace, act_space_reps)
         # G-invariant critic
         self.critic_in_type = FieldType(gspace, obs_space_reps_critic)
+        self.critic_out_type = FieldType(gspace, [self.G.trivial_representation])
 
         # Policy funciton parameterizes a G-equivariant Multivariate Normal distribution
-        self.action_gaussian = EquivMultivariateNormal(y_type=self.actor_out_type)
+        # self.action_gaussian = EquivMultivariateNormal(y_type=self.actor_out_type)
+        self.std = nn.Parameter(init_noise_std * torch.ones(num_actions))
 
-        self.actor = EMLP(
-            in_type=self.actor_in_type,
-            out_type=self.action_gaussian.in_type,  # Parameters if the equiv Gaussian
-            bias=True,
-            hidden_units=actor_hidden_dims,
-            activation=activation,
-        )
+        # self.actor = EMLP(
+        #     in_type=self.actor_in_type,
+        #     out_type=self.action_gaussian.in_type,  # Parameters if the equiv Gaussian
+        #     bias=True,
+        #     hidden_units=actor_hidden_dims,
+        #     activation=activation,
+        # )
 
-        self.critic = IMLP(
-            in_type=self.critic_in_type,
-            out_dim=1,  # Invariant scalar Value function
-            bias=True,
-            hidden_units=critic_hidden_dims,
-            activation=activation,
-        )
+        # self.critic = IMLP(
+        #     in_type=self.critic_in_type,
+        #     out_dim=1,  # Invariant scalar Value function
+        #     bias=True,
+        #     hidden_units=critic_hidden_dims,
+        #     activation=activation,
+        # )
+
+        self.actor = SimpleEMLP(self.actor_in_type, self.actor_out_type,
+            hidden_dims=actor_hidden_dims,
+            activation=activation)
+
+        self.critic = SimpleEMLP(self.critic_in_type, self.critic_out_type,
+            hidden_dims = critic_hidden_dims,
+            activation=activation,)
 
         print(f"Critic MLP: {self.critic}")
 
@@ -95,7 +105,8 @@ class ActorCriticSymm(ActorCritic):
         print(f"Critic [{params / 1e6:.2f}M params]: \n{self.critic}")
 
         # Action distribution (populated in update_distribution)
-        self.distribution: torch.distributions.MultivariateNormal = None
+        # self.distribution: torch.distributions.MultivariateNormal = None
+        self.distribution: torch.distributions.Normal = None
         # disable args validation for speedup
         Normal.set_default_validate_args(False)
 
@@ -118,7 +129,8 @@ class ActorCriticSymm(ActorCritic):
         """Returns the entropy of the action distribution conditioned on last observations."""
         if self.distribution is None:
             raise ValueError("Distribution not updated. Call update_distribution() first.")
-        return self.distribution.entropy()
+        # return self.distribution.entropy()
+        return self.distribution.entropy().sum(dim=-1)
 
     def get_actions_log_prob(self, actions: torch.Tensor):
         """Returns the log probability of the given actions under the current action distribution.
@@ -129,7 +141,7 @@ class ActorCriticSymm(ActorCritic):
         Returns:
             torch.Tensor: Log probabilities of the actions (batch_size, 1).
         """
-        probs = self.distribution.log_prob(actions)
+        probs = self.distribution.log_prob(actions).sum(dim=-1)
         assert probs.shape[0] == actions.shape[0]
         return probs
 
@@ -145,8 +157,10 @@ class ActorCriticSymm(ActorCritic):
             observations = observations["policy"]
 
         observations = self.actor_in_type(observations)
-        dist_params = self.actor(observations)
-        self.distribution = self.action_gaussian.get_distribution(dist_params)
+        # dist_params = self.actor(observations)
+        # self.distribution = self.action_gaussian.get_distribution(dist_params)
+        mean = self.actor(observations).tensor
+        self.distribution = Normal(mean, mean*0. + self.std)
 
     def act_inference(self, observations):
         """Returns the mean action for the given observations during inference."""
@@ -156,7 +170,7 @@ class ActorCriticSymm(ActorCritic):
 
         observations = self.actor_in_type(observations)
         dist_params = self.actor(observations)
-        actions_mean = dist_params.tensor[..., : self.actor_out_type.size]
+        actions_mean = dist_params.tensor#[..., : self.actor_out_type.size]
         return actions_mean
 
     def evaluate(self, critic_observations, **kwargs):
@@ -186,3 +200,64 @@ class ActorCriticSymm(ActorCritic):
     def update_normalization(self, observations):
         """Override base ActorCritic to prevent missing normalization attribute errors."""
         pass
+
+class SimpleEMLP(EquivariantModule):
+    def __init__(self,
+                 in_type: FieldType,
+                 out_type: FieldType,
+                 hidden_dims = [256, 256, 256],
+                 bias: bool = True,
+                 actor: bool = True,
+                 activation: str = "ReLU"):
+        super().__init__()
+        self.out_type = out_type
+        gspace = in_type.gspace
+        group = gspace.fibergroup
+
+        layer_in_type = in_type
+        self.net = escnn.nn.SequentialModule()
+        for n in range(len(hidden_dims)):
+            layer_out_type = FieldType(gspace, [group.regular_representation] * int((hidden_dims[n] / group.order())))
+
+            self.net.add_module(f"linear_{n}: in={layer_in_type.size}-out={layer_out_type.size}",
+                             escnn.nn.Linear(layer_in_type, layer_out_type, bias=bias))
+            self.net.add_module(f"act_{n}", self.get_activation(activation, layer_out_type))
+
+            layer_in_type = layer_out_type
+
+        if actor:
+            self.net.add_module(f"linear_{len(hidden_dims)}: in={layer_in_type.size}-out={out_type.size}",
+                                escnn.nn.Linear(layer_in_type, out_type, bias=bias))
+            self.extra_layer = None
+        else:
+            num_inv_features = len(layer_in_type.irreps)
+            self.extra_layer = torch.nn.Linear(num_inv_features, out_type.size, bias=False)
+
+    def forward(self, x: GeometricTensor) -> GeometricTensor:
+        x= self.net(x)
+        if self.extra_layer:
+            x = self.extra_layer(x.tensor)
+        return x
+
+    @staticmethod
+    def get_activation(activation: str, hidden_type: FieldType) -> EquivariantModule:
+        if activation.lower() == "relu":
+            return escnn.nn.ReLU(hidden_type)
+        elif activation.lower() == "elu":
+            return escnn.nn.ELU(hidden_type)
+        elif activation.lower() == "lrelu":
+            return escnn.nn.LeakyReLU(hidden_type)
+        else:
+            raise NotImplementedError
+
+    def evaluate_output_shape(self, input_shape):
+        """Returns the output shape of the model given an input shape."""
+        batch_size = input_shape[0]
+        return batch_size, self.out_type.size
+
+    def export(self):
+        """Exports the model to a torch.nn.Sequential instance."""
+        sequential = nn.Sequential()
+        for name, module in self.net.named_children():
+            sequential.add_module(name, module.export())
+        return sequential
